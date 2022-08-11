2aa0406d71a1660b336544cfd90d95a288ee4a8c
Martin <m.rodriguez0743@gmail.com>
2022-08-10 22:41:36 -0400

    Add "greet" function to button element

---
+++ script.js
@@ -0,0 +1,14 @@
+// creates variable 'button'
+// value is the 'button' element in the document object (HTML file)
+const button = document.querySelector('button');
+
+// a function that requires input from user through a prompt
+// once user creates input, an alert with a new message appears
+function greet() {
+    const name = prompt('What\'s your name friend?');
+    alert(`Hello ${name}, welcome to this useless page of JS tidbits!`);
+};
+
+// chains an event listener to variable 'button'
+// when user clicks button, the greet function runs
+button.addEventListener('click', greet);
